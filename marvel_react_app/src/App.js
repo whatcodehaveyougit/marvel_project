a1b50361f9a60f995224ce7aab8008280f086042
whatcodehaveyougit <siggywatt@yahoo.com>
2022-09-19 20:35:31 +0200

    Do not need to re-build image manually after doing a refresh, using volumes

--- marvel_react_app/src/App.js
+++ marvel_react_app/src/App.js
@@ -2,12 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const apiUrl = process.env.API_URL;
+  console.log(apiUrl);
+
+  console.log('hello');
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reeeeload. To the date version.
         </p>
         <a
           className="App-link"

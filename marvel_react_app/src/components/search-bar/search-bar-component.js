799efed653b0bc8d99f0d4555a75aafef4b110e1
whatcodehaveyougit <siggywatt@yahoo.com>
2022-09-22 18:49:07 +0200

    Can now search characters by name, so MVP now reached!

---
+++ marvel_react_app/src/components/search-bar/search-bar-component.js
@@ -0,0 +1,17 @@
+// import {
+//     TextField 
+// } from '@mui/material/';
+// import './search-bar.styles.scss'
+
+// const SearchBar = () => {
+
+//     return (
+//         <div className="search-bar-component">
+//             <TextField
+//                 placeholder="Search Marvel Characters!"
+//                 />
+//         </div>
+//     )
+// }
+
+// export default SearchBar;
--- marvel_react_app/src/routes/home/home.component.js
+++ marvel_react_app/src/routes/home/home.component.js
@@ -1,15 +1,39 @@
-import { Grid } from '@mui/material';
+import { 
+    Grid,
+    TextField
+ } from '@mui/material';
 import './home.styles.scss'
 import CharacterCard from '../../components/character-card/character-card.componet'
+import SearchBar from '../../components/search-bar/search-bar-component'
+import { useEffect, useState } from 'react'
 
 const Home = ( { charactersData } ) => {
 
+    const [ charactersDataState, setCharctersDataState ] = useState();
+
+    const handleChange = (e) => {
+        const filteredCharacters = charactersData.filter(character => character.name.includes(e.target.value));
+        setCharctersDataState( filteredCharacters )
+    }
+
+    useEffect(() => {
+        if ( charactersData ) {
+            setCharctersDataState( charactersData )
+        }
+    }, [charactersData] )
 
     return (
         <div className="home-container">
+            <div className="search-bar-component">
+                <TextField 
+                placeholder="Search Marvel Characters!" 
+                name="firstName" 
+                onChange={handleChange}
+            />
+            </div>
             <Grid container spacing={2}>
                 {
-                    charactersData && charactersData.map(( character ) => (
+                    charactersDataState && charactersDataState.map(( character ) => (
                     <Grid item xs={12} sm={6} md={4} className="character-card-container" key={character.id}>
                         <CharacterCard character={character} />
                     </Grid>

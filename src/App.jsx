1829d0aeebf27df4036fc97ef400c18fd8a2a1a9
whatcodehaveyougit <sigurdwatt@gmail.com>
2024-03-10 17:51:44 +0000

    Fix console warnings

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,36 @@
-import { Route, Routes } from 'react-router-dom';
-import Nav from './components/nav/nav.component'
-import Home from './routes/home/home.component'
-import About from './routes/about/about.component'
-import Character from './routes/character/character.component'
-import { Grid } from '@mui/material';
-import { fetchData } from './utils/utils'
-import { useState, useEffect } from 'react'
-
+import { Route, Routes } from "react-router-dom";
+import Nav from "./components/nav/nav.component";
+import Home from "./routes/home/home.component";
+import About from "./routes/about/about.component";
+import Character from "./routes/character/character.component";
+import { Grid } from "@mui/material";
+import { fetchData } from "./utils/utils";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [charactersData, setCharacters] = useState([]);
 
-  const [ charactersData, setCharacters ] = useState([])
-
-  useEffect(  () => {
-      const fetchPageData = async () => {
-          const result = await fetchData( '/characters' )
-          setCharacters( result['data']['results'] );
-      }
-      fetchPageData()
-          .catch(console.error)
-  }, [])
-
+  useEffect(() => {
+    const fetchPageData = async () => {
+      const result = await fetchData("/characters");
+      setCharacters(result["data"]["results"]);
+    };
+    fetchPageData().catch(console.error);
+  }, []);
 
   return (
     <>
       <Grid margin={2}>
         <Nav />
         <Routes>
-            <Route path='/' element={<Home charactersData={charactersData} />} />
-            <Route path='about' element={<About />} />
-            <Route path='/character/:characterid' element={<Character charactersData={charactersData} />} />
+          <Route path="/" element={<Home charactersData={charactersData} />} />
+          <Route path="about" element={<About />} />
+          <Route
+            path="/character/:characterid"
+            element={<Character charactersData={charactersData} />}
+          />
         </Routes>
       </Grid>
-
     </>
   );
 }

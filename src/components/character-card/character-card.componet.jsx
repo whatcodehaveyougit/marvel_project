866dd2962da3be5a82bd17c2dbe48783dfde5743
whatcodehaveyougit <sigurdwatt@gmail.com>
2024-03-29 10:21:55 +0000

    Refactor

--- src/components/character-card/character-card.componet.jsx
+++ src/components/character-card/character-card.componet.jsx
@@ -7,30 +7,30 @@ const CharacterCard = ({ character }) => {
     useState(null);
   const containerDiv = useRef(null);
   const contentDiv = useRef(null);
-
   useEffect(() => {
-    const container = containerDiv;
-    const content = contentDiv;
+    const container = containerDiv.current;
+    const content = contentDiv.current;
+    if (content && container) {
+      const containerClientHeight = container.clientHeight;
+      const contentScrollHeight = content.scrollHeight;
 
-    if (content.current !== null && container.current !== null) {
-      const containerClientHeight = container.current.clientHeight;
-      const contentScrollHeight = content.current.scrollHeight;
+      // Why are these not showing the heights of the elements as expected?
+      // But the code still works.
+      // console.log(containerClientHeight);
+      // console.log(contentScrollHeight);
 
       if (contentScrollHeight > containerClientHeight) {
         const visibleCharacters = Math.floor(
           (containerClientHeight / contentScrollHeight) *
-            content.current.textContent.length
+            (content.textContent.length - 3) // Adjust for the length of the ellipsis
         );
         setNumberOfCharactersToShow(visibleCharacters);
-      } else {
-        console.log("Content is fully visible");
       }
     }
-  }, [contentDiv, numberOfCharactersToShow]);
+  }, []);
 
   function showFirstNCharacters(string, n) {
     if (n) {
-      console.log(string.slice(0, n));
       return string.slice(0, n) + "...";
     }
     return string;
@@ -40,7 +40,7 @@ const CharacterCard = ({ character }) => {
     <div className="character-card relative h-full text-center">
       <img
         className="w-full h-120 object-cover"
-        src={`${character["thumbnail"]["path"]}.${character["thumbnail"]["extension"]}`}
+        src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
         alt={character.name}
       />
       <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-full bg-white">
@@ -53,7 +53,7 @@ const CharacterCard = ({ character }) => {
           >
             {character.description && (
               <div className="text-gray-500">
-                <p ref={contentDiv} id={character.id}>
+                <p ref={contentDiv} id={`contentdiv-${character.id}`}>
                   {showFirstNCharacters(
                     character.description,
                     numberOfCharactersToShow

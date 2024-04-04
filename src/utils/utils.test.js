c151aa525b91c45b4a6ac9be2c28930a80e7a246
whatcodehaveyougit <sigurdwatt@gmail.com>
2024-04-04 09:05:18 +0100

    Write unit tests for utils file

--- src/utils/utils.test.js
+++ src/utils/utils.test.js
@@ -1,52 +1,74 @@
-import { fetchData, generateBackgroundImageUrl, showFirstNCharacters } from './utils';
+import { fetchData } from './utils';
 import MD5 from 'crypto-js/md5';
+import { generateBackgroundImageUrl, showFirstNCharacters } from './utils';
 
 // Mock the MD5 function from the crypto-js library
 jest.mock('crypto-js/md5', () => ({
-  toString: jest.fn(),
+  __esModule: true,
+  default: jest.fn(), // Mock MD5 as a function
 }));
 
 describe('fetchData function', () => {
   beforeEach(() => {
     jest.clearAllMocks();
+
+    // Mock environment variables
+    process.env.REACT_APP_API_URL = 'mock_api_url';
+    process.env.REACT_APP_PRIVATE_KEY = 'mock_private_key';
+    process.env.REACT_APP_PUBLIC_KEY = 'mock_public_key';
   });
 
-  // it('fetches data successfully', async () => {
-  //   const mockResponse = { data: 'mock data' };
-  //   const mockJsonPromise = Promise.resolve(mockResponse);
-  //   const mockFetchPromise = Promise.resolve({
-  //     json: () => mockJsonPromise,
-  //   });
-  //   global.fetch = jest.fn().mockImplementation(() => mockFetchPromise);
+  afterEach(() => {
+    // Reset environment variables after each test
+    delete process.env.REACT_APP_API_URL;
+    delete process.env.REACT_APP_PRIVATE_KEY;
+    delete process.env.REACT_APP_PUBLIC_KEY;
+  });
+  it('fetches data successfully', async () => {
+    // Mock MD5 function to return the expected hash
+    MD5.mockReturnValueOnce({
+      toString: jest.fn().mockReturnValue('mockedHash'),
+    });
+    const ts = Date.now();
+    global.Date.now = jest.fn(() => ts);
 
-  //   const apiRoute = '/test-route';
-  //   const result = await fetchData(apiRoute);
+    const mockResponse = { data: 'mock data' };
+    const mockJsonPromise = Promise.resolve(mockResponse);
+    const mockFetchPromise = Promise.resolve({
+      json: () => mockJsonPromise,
+    });
+    global.fetch = jest.fn().mockResolvedValueOnce(mockFetchPromise);
 
-  //   expect(global.fetch).toHaveBeenCalledWith(`${process.env.REACT_APP_API_URL}${apiRoute}?ts=`, {
-  //     method: 'GET',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //   });
-  //   expect(result).toEqual(mockResponse);
-  // });
+    const apiRoute = '/test-route';
+    const result = await fetchData(apiRoute);
 
-  // it('handles fetch error', async () => {
-  //   const mockError = new Error('Mock fetch error');
-  //   global.fetch = jest.fn().mockRejectedValue(mockError);
+    // Ensure MD5 was called with the correct parameters
+    expect(MD5).toHaveBeenCalledWith(expect.any(String));
+    expect(global.fetch).toHaveBeenCalledWith(`${process.env.REACT_APP_API_URL}${apiRoute}?ts=${ts}&apikey=${process.env.REACT_APP_PUBLIC_KEY}&hash=mockedHash`)
+    expect(result).toEqual(mockResponse);
+  });
 
-  //   const apiRoute = '/test-route';
-  //   const result = await fetchData(apiRoute);
+  it('handles fetch error', async () => {
+    // Mock MD5 function to return the expected hash
+    MD5.mockReturnValueOnce({
+      toString: jest.fn().mockReturnValue('mockedHash'),
+    });
+    const ts = Date.now();
+    global.Date.now = jest.fn(() => ts);
 
-  //   expect(global.fetch).toHaveBeenCalledWith(`${process.env.REACT_APP_API_URL}${apiRoute}?ts=`, {
-  //     method: 'GET',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //   });
-  //   expect(result).toEqual(`Fetch Failed: ${mockError}`);
-  // });
-});
+    const mockError = new Error('Mock fetch error');
+    global.fetch = jest.fn().mockRejectedValueOnce(mockError);
+
+    const apiRoute = '/test-route';
+    const result = await fetchData(apiRoute);
+
+    // Ensure MD5 was called with the correct parameters
+    expect(MD5).toHaveBeenCalledWith(expect.any(String));
+    expect(global.fetch).toHaveBeenCalledWith(`${process.env.REACT_APP_API_URL}${apiRoute}?ts=${ts}&apikey=${process.env.REACT_APP_PUBLIC_KEY}&hash=mockedHash`)
+
+    expect(result).toEqual(`Fetch Failed: ${mockError}`);
+  });
+})
 
 describe('generateBackgroundImageUrl function', () => {
   it('returns empty string when characterData is falsy', () => {
@@ -82,4 +104,7 @@ describe('showFirstNCharacters function', () => {
     const expected = 'test...';
     expect(showFirstNCharacters(string, n)).toEqual(expected);
   });
+
 });
+
+
